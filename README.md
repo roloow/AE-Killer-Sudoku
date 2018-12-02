2372297842ac8a5c0d759c5b80c867ffb9e2828b
rolandocq <rolando.casanueva.12@sansano.usm.cl>
2018-12-02 20:44:33 -0300

    first main

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-ProyectoIA
+# ProyectoIA
+
---
+++ main.cpp
@@ -0,0 +1,26 @@
+// *********************************************************************
+//                            LIBRERIAS
+// *********************************************************************
+
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <curses.h>
+#include <iostream>
+
+// *********************************************************************
+//                       CONSTANTES GLOBALES
+// *********************************************************************
+
+/* Tamaño de la población */
+#define TAM_POB 13
+
+/* Tamaño del cromozoma */
+#define TAM_CROM 81
+
+/* Probabilidad de cruzamiento */
+#define PROB_CRU 0.33
+
+/* Probabilidad de mutación */
+#define PROB_MUT 0.33
